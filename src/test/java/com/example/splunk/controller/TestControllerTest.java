7214915f3d7563ba74a9bbf187154040735986f0
Gokulraj A <134777980+Gokulraj18@users.noreply.github.com>
2026-03-19 11:16:27 +0530

    Chnages in test

--- src/test/java/com/example/splunk/controller/TestControllerTest.java
+++ src/test/java/com/example/splunk/controller/TestControllerTest.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
